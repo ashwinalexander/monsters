d7ac29827febbc7b46467d8797a2f4e6fd06bff1
ashwin alexander <alexwin75@gmail.com>
2022-11-16 22:00:55 -0500

    adding typescript

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,10 +1,10 @@
-import './card.styles.css';
+import "./card.styles.css";
 
 const Card = ({ monster }) => {
   const { id, name, email } = monster;
 
   return (
-    <div className='card-container' key={id}>
+    <div className="card-container" key={id}>
       <img
         src={`https://robohash.org/${id}?set=set2&size=180x180`}
         alt={`monster ${name}`}
--- src/components/search-box/search-box.component.tsx
+++ src/components/search-box/search-box.component.tsx
@@ -1,10 +1,10 @@
-import './search-box.styles.css';
+import "./search-box.styles.css";
 
 //func component: no internal state or access to lifecycle methods
 export const SearchBox = ({ placeholder, handleChange, className }) => (
   <input
     className={className}
-    type='search'
+    type="search"
     placeholder={placeholder}
     onChange={handleChange}
   />
